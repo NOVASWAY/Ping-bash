72a6a3d669866cf9be0788a44dd5dfdb64d55703
NOVASWAY <njauwangari6@gmail.com>
2026-02-02 10:41:48 +0300

    Added monitor functions for CPU and memory usage

--- monitor.sh
+++ monitor.sh
@@ -8,4 +8,19 @@ send_alert() {
     echo "$(tput setaf 1) ALERT: usage exceeded threshold! Current value: $2% $(tput sgr0)"
 }
 
-send_alert "CPU" 85 # Test value to test functionality.
+# Monitor CPU usage
+cpu_usage=$(top -bn1 | grep "Cpu(s)" | awk '{print $2 + $4}')
+cpu_usage=${cpu_usage%.*}
+echo "Current CPU Usage: $cpu_usage%"
+
+if  ((cpu_usage >= CPU_THRESHOLD)); then
+    send_alert "CPU" "$cpu_usage"
+fi
+
+# Monitor Memory usage
+memory_usage=$(free | awk  '/Mem/ {printf("%3.if", ($3/$2) * 100.0)}')
+echo "Current Memory Usage: $memory_usage%"
+memory_usage=${memory_usage%.*}
+if  ((memory_usage >= MEMORY_THRESHOLD)); then
+    send_alert "Memory" "$memory_usage"
+fi
